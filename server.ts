92b8c4ca31d02dc484cbc1dcc88c8b1225cef604
ScottMBerger <contact@scottb.app>
2019-02-02 22:21:39 -0500

    add express server with prerender.io

---
+++ server.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as prerender from 'prerender-node';
+
+const app: express.Application = express();
+const port: string | number = process.env.PORT || 3000;
+
+app.use(prerender);
+app.use(express.static(path.join(__dirname, '/dist/folio')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/folio/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/`);
+});
